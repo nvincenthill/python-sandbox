454cfc0027876dda244f91d5a322ed160ef7cac2
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-01-08 15:37:34 -0800

    Implement list example with append and count

---
+++ list.py
@@ -0,0 +1,10 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+
+# append an item
+fruits.append('watermellon')
+
+# count an item
+apple_count = fruits.count('apple')
+
+print(fruits)
+print(apple_count)
