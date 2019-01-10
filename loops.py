f9aec3738d8ec4a31186e011e10be1f07510e631
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-01-10 09:49:19 -0800

    Implement example loops

---
+++ loops.py
@@ -0,0 +1,8 @@
+knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+# use items() to access each key/value pair in dictionary
+for k, v in knights.items():
+    print(k, v)
+
+# enumerate over list to access each value/index
+for i, v in enumerate(['tic', 'tac', 'toe']):
+    print(i, v)
