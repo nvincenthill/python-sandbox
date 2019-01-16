12cc7d61b3bf0e733502c9ea97f3ecc40603a54b
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-01-16 09:50:06 -0800

    Implement MVP find_odd_int

---
+++ find_odd_int.py
@@ -0,0 +1,13 @@
+def find_odd_int(seq):
+    tally = {}
+    for i in seq:
+        if i in tally:
+            tally[i] = False
+        else:
+            tally[i] = True
+    for j in tally:
+        if tally[j]:
+            return j
+
+
+print(find_odd_int([1, 2, 3, 1, 2]))
